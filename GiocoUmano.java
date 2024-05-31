712a28a70de5bc8873537afd2642edbf983c8666
LauGav07 <160546767+LauGav07@users.noreply.github.com>
2024-05-31 14:18:23 +0200

    Update GiocoUmano.java

--- GiocoUmano.java
+++ GiocoUmano.java
@@ -1,24 +1,36 @@
-import java.util.Scanner;
-import java.awt.Frame;
-
-public class GiocoUmano  {
-
-    public static void main(String[]args) {
-        Finestre.main(args);
-        Metodi.svuota();
-        System.out.println("con cosa vuoi giocare? 1 = x; -1 = o");
-        Metodi.conCosaGioca1 = Metodi.chiediInt(2);
-        boolean rifare =true;
+public class GiocoUmano {
+    public static void main(String[] args) {
         do {
+            //Finestre.main(args);
+            Metodi.svuota();
+            boolean tr = true;
+            do {
+                tr= true;
+            System.out.println("con cosa vuoi giocare?  X oppure O");
+            String b = Metodi.chiedistringa() ;
+            String assoc = b.toLowerCase();
+            if  (assoc.equals("x")){
+                Metodi.conCosaGioca1=1;
+            } else if (assoc.equals("o")){
+                Metodi.conCosaGioca1=-1;
+            }
+            else{
+                System.out.println("errore");
+                tr = false;
+            } }while(!tr);
+
+            Metodi.rifare = true;
             Metodi.tris();
+            //Finestre.main(args);
             int inizio = Metodi.inizio();
-            System.out.println("turno di :"+inizio);
-            if (inizio == Metodi.conCosaGioca1 ) {
+            System.out.println("turno di :" + inizio);
+            if (inizio == Metodi.conCosaGioca1) {
                 do {
                     System.out.println("TURNO GIOCATORE1");
                     gioco(1);
                     Metodi.tris();
-                    Metodi.fine();
+                    //Finestre.main(args);
+                    Metodi.fine(1);
 
                     if (Metodi.ctrl == true || Metodi.part == true) {
                         break;
@@ -26,17 +38,18 @@ public class GiocoUmano  {
                     System.out.println("TURNO GIOCATORE2");
                     gioco(2);
                     Metodi.tris();
-                    Metodi.fine();
+                    //Finestre.main(args);
+                    Metodi.fine(1);
 
                 }
-                while(!Metodi.ctrl && !Metodi.part);
-            }
-            else {
+                while (!Metodi.ctrl && !Metodi.part);
+            } else {
                 do {
                     System.out.println("TURNO GIOCATORE2");
                     gioco(2);
                     Metodi.tris();
-                    Metodi.fine();
+                    //Finestre.main(args);
+                    Metodi.fine(1);
 
                     if (Metodi.ctrl == true || Metodi.part == true) {
                         break;
@@ -44,28 +57,83 @@ public class GiocoUmano  {
                     System.out.println("TURNO GIOCATORE1");
                     gioco(1);
                     Metodi.tris();
-                    Metodi.fine();
+                    //Finestre.main(args);
+                    Metodi.fine(1);
 
-                }while(Metodi.ctrl == false && Metodi.part == false);
+                } while (Metodi.ctrl == false && Metodi.part == false);
             }
-            rifare =Metodi.rifare();
-            Metodi.svuota();
-        } while (rifare==true);
+            Metodi.rifare = Metodi.rifare();
+        } while (Metodi.rifare == true);
     }
-
-
-
-
-    public static void gioco(int chiGioca){
-
-        System.out.println("in che riga?");
-        int riga = Metodi.chiediInt(3);
-        System.out.println("in che colonna?");
-        int colonna = Metodi.chiediInt(3);
-        if (Main.tris[riga][colonna]==0){
-            if (chiGioca == 1){
+    public static void gioco(int chiGioca) {
+        int colonna = -5;
+        int riga = -5;
+        switch (Main.tastiera) {
+            case -1:
+                boolean cont;
+                do {
+                    System.out.println("in che riga?");
+                    riga = Metodi.chiediInt();
+                    cont = true;
+                    System.out.println("in che colonna? (se vuoi cambiare la riga, scrivere cambio)");
+                    colonna = Metodi.chiediInt();
+                    if (colonna == 10) {
+                        cont = false;
+                    }
+                } while (!cont);
+                break;
+            case -2, -3:
+                String numero = Metodi.chiedistringa();
+                switch (numero) {
+                    case "1", "z":
+                        riga = 2;
+                        colonna = 0;
+                        break;
+                    case "2", "x":
+                        riga = 2;
+                        colonna = 1;
+                        break;
+                    case "3", "c":
+                        riga = 2;
+                        colonna = 2;
+                        break;
+                    case "4", "a":
+                        riga = 1;
+                        colonna = 0;
+                        break;
+                    case "5", "s":
+                        riga = 1;
+                        colonna = 2;
+                        break;
+                    case "6", "d":
+                        riga = 1;
+                        colonna = 2;
+                        break;
+                    case "7", "q":
+                        riga = 0;
+                        colonna = 0;
+                        break;
+                    case "8", "w":
+                        riga = 0;
+                        colonna = 1;
+                        break;
+                    case "9", "e":
+                        riga = 0;
+                        colonna = 2;
+                        break;
+                    default:
+                        System.out.println("inesistente, riprova");
+                        gioco(chiGioca);
+                        break;
+                }
+                System.out.println("riga" + riga);
+                System.out.println("colonna = " + colonna);
+                break;
+        }
+        if (Main.tris[riga][colonna] == 0) {
+            if (chiGioca == 1) {
                 Main.tris[riga][colonna] = Metodi.conCosaGioca1;
-            }else{
+            } else {
                 Main.tris[riga][colonna] = Metodi.conCosaGioca1 * -1;
             }
         } else {
@@ -73,5 +141,4 @@ public class GiocoUmano  {
             gioco(chiGioca);
         }
     }
-
 }
